2c67528b4120e90a29c80c53e7957e9e46377c7d
Piotr Pesta <piotr.pesta@gmail.com>
2017-08-24 21:00:51 +0200

    Initial commit

---
+++ app/src/main/java/pl/smartfan/adrian/GameActivity.java
@@ -0,0 +1,8 @@
+package pl.smartfan.adrian;
+
+/**
+ * Created by Piotr on 24.08.2017.
+ */
+
+public class GameActivity {
+}
---
+++ app/src/main/java/pl/smartfan/adrian/MainActivity.java
@@ -0,0 +1,49 @@
+package pl.smartfan.adrian;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer backgroundMusicMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //fade in game title
+        ImageView titleImage = (ImageView) findViewById(R.id.titleImage);
+        Animation titleFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        titleImage.startAnimation(titleFadeInAnimation);
+
+        //adding start button
+        //ImageButton startButton = (ImageButton) findViewById(R.id.startButton);
+
+        //play background music
+        backgroundMusicMain = MediaPlayer.create(this, R.raw.splash_music);
+        backgroundMusicMain.start();
+    }
+
+    public void startButton() {
+        //starting game activity
+        startActivity(new Intent(this, GameActivity.class));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        backgroundMusicMain.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        backgroundMusicMain.start();
+    }
+}
